dda7cc100b5f21f0137d95146c53cd04cacdd28f
LxxsCLN <113736900+LxxsCLN@users.noreply.github.com>
2023-02-16 12:12:02 -0600

    add button to choose size

--- styles/scripts.js
+++ styles/scripts.js
@@ -1,5 +1,6 @@
 let grid = document.getElementById("grid");
 let erase = document.getElementById("erase");
+let size = 16;
 
 function createGrid()
 {
@@ -9,8 +10,23 @@ function createGrid()
         grid.appendChild(squarez);
         squarez.classList.add("squares");
     }
+    grid.style.setProperty("grid-template-columns", "repeat(" + size + ", 1fr)");
 }
 createGrid();
 grid.addEventListener("mouseover", (e) => {e.target.style.backgroundColor = "red";});
-erase.addEventListener("click", () => {grid.innerHTML = ""; createGrid();});
 
+sizebtn.addEventListener("click", () => {let size = prompt("Choose size"); createGrid2(size)});
+erase.addEventListener("click", () => {grid.innerHTML = ""; createGrid2(size);});
+function createGrid2(n)
+{
+    grid.innerHTML = "";
+    for (let i = 0; i < (n*n); i++)
+    {
+        let squarez = document.createElement("div");
+        grid.appendChild(squarez);
+        squarez.classList.add("squares");
+    }
+    grid.style.setProperty("grid-template-columns", "repeat(" + n + ", 1fr)");
+}
+
+/* can delete first function. also need to fix erase button. it erases and goes back to 16x16 */ 
