7c07ddd4aeaf7f486099798fcd929d63899a4ba2
LxxsCLN <113736900+LxxsCLN@users.noreply.github.com>
2023-02-16 20:53:23 -0600

    clean code, erase now leave the grid as it is instead of reverting back to 16 x 16

--- styles/scripts.js
+++ styles/scripts.js
@@ -2,22 +2,24 @@ let grid = document.getElementById("grid");
 let erase = document.getElementById("erase");
 let size = 16;
 
-function createGrid()
-{
-    for (let i = 0; i < 16*16; i++)
-    {
-        let squarez = document.createElement("div");
-        grid.appendChild(squarez);
-        squarez.classList.add("squares");
-    }
-    grid.style.setProperty("grid-template-columns", "repeat(" + size + ", 1fr)");
-}
-createGrid();
 grid.addEventListener("mouseover", (e) => {e.target.style.backgroundColor = "red";});
 
-sizebtn.addEventListener("click", () => {let size = prompt("Choose size"); createGrid2(size)});
-erase.addEventListener("click", () => {grid.innerHTML = ""; createGrid2(size);});
-function createGrid2(n)
+sizebtn.addEventListener("click", ccss);
+erase.addEventListener("click", eerr);
+
+function ccss()
+{
+    size = prompt("Choose size"); 
+    createGrid(size);
+}
+
+function eerr()
+{
+    grid.innerHTML = ""; 
+    createGrid(size);
+}
+
+function createGrid(n)
 {
     grid.innerHTML = "";
     for (let i = 0; i < (n*n); i++)
@@ -29,4 +31,4 @@ function createGrid2(n)
     grid.style.setProperty("grid-template-columns", "repeat(" + n + ", 1fr)");
 }
 
-/* can delete first function. also need to fix erase button. it erases and goes back to 16x16 */ 
+createGrid(size);
