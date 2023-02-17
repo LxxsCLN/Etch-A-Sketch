598d58672728501d83fa8fa4118f2fe5117b6f67
LxxsCLN <113736900+LxxsCLN@users.noreply.github.com>
2023-02-17 11:52:56 -0600

    fix format, default tiles is now 30.

--- styles/scripts.js
+++ styles/scripts.js
@@ -1,7 +1,30 @@
 let grid = document.getElementById("grid");
-let size = 16;
+let size = 30;
 
-grid.addEventListener("mouseover", (e) => {rando =  Math.floor(Math.random()*16777215).toString(16); e.target.style.backgroundColor = "#" + rando;});
+
+function paintGrid(a, color){    
+    if(a.buttons == 1){
+        if(a.target.classList == "squares"){
+            let squarezz = a.target;    
+            squarezz.style.backgroundColor = color;
+        }  
+    }else{
+        //Exit condition if mouse is not clicked.
+        return;
+    }
+}
+
+//Returns a random rgb color.
+function getRandomRgb() {
+    let rando =  Math.floor(Math.random()*16777215).toString(16);
+    return "#" + rando;
+}
+
+//paintGrid when mouseover event in selectedColor
+grid.addEventListener('mousedown', e =>{paintGridEvent = paintGrid(e, getRandomRgb()); if(e.buttons == 1)
+                                                                                                {        
+                                                                                                window.addEventListener('mouseover', (e) => {paintGrid(e, getRandomRgb());});
+                                                                                                }});
 
 sizebtn.addEventListener("click", ccss);
 
